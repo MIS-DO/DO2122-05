928558d293261786b7156fab22888a90a8baaacf
Sergio Sánchez León <56033905+sersanleo@users.noreply.github.com>
2022-03-15 19:28:51 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ export mongoDBHostname=mongo
 export mongoLog=mongodb1.log
 export dataUrl=db2
 export logsUrl=logs1
-docker-compose -p acme-explorer-g5-development up --build -d
+sudo docker-compose -p acme-explorer-g5-development up --build -d
 
 # production
 export NODE_ENV=production
@@ -16,4 +16,4 @@ export mongoDBHostname=mongo
 export mongoLog=mongodb2.log
 export dataUrl=db2
 export logsUrl=logs2
-docker-compose -p acme-explorer-g5-production up --build -d
+sudo docker-compose -p acme-explorer-g5-production up --build -d
